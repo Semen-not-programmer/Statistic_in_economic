7a9c0f06efe7fdb1105eb00eebd0b4034f561505
Semen <semenkosovskiy@gmail.com>
2025-08-22 20:07:43 +0300

    Новая структура файлов

---
+++ discrete.py
@@ -0,0 +1,8 @@
+# в этом файле считываются характеристики дискретной случайной величины
+
+from typing import Any, List, Union
+from typing import Union
+import math
+import os
+FILENAME = os.path.basename(__file__)
+
--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-erdjnnj
+# этот файл проверяет правильность функционирования программы
+#
+#
+
+import os
+FILENAME = os.path.basename(__file__)
+
+class_name = "W:\Текстовые документы\Курс Методы анализа экономики\Литература\statistic.py"
+
+from selection import Statistic
+
+
+t = Statistic([10, 41, 485])
+print(t)
